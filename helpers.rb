14f84495fa2e1d7557a18144a5322716fa84ee8b
Maimer <nesnl23@yahoo.com>
2016-12-31 15:31:47 -0800

    Update gems, fix redis URL to work with heroku new stack

--- helpers.rb
+++ helpers.rb
@@ -3,8 +3,8 @@ require 'redis'
 
 def get_connection
   begin
-    if ENV.has_key?("REDISCLOUD_URL")
-      connection = Redis.new(url: ENV["REDISCLOUD_URL"])
+    if ENV.has_key?("REDIS_URL")
+      connection = Redis.new(url: ENV["REDIS_URL"])
     else
       connection = Redis.new
     end
@@ -16,9 +16,7 @@ end
 def find_game(id)
   redis = get_connection
   game_check = redis.get(id)
-  if game_check.nil?
-    return nil
-  end
+  return nil if game_check.nil?
   game = JSON.parse(game_check, symbolize_names: true)
   redis.quit
   game
@@ -26,12 +24,17 @@ end
 
 def save_game(id, word, hidden, letters, guesses, wins, losses)
   redis = get_connection
-  redis.set(id, { word: word,
-                  hidden: hidden,
-                  letters: letters,
-                  guesses: guesses,
-                  wins: wins,
-                  losses: losses }.to_json)
+  redis.set(
+    id,
+    {
+      word: word,
+      hidden: hidden,
+      letters: letters,
+      guesses: guesses,
+      wins: wins,
+      losses: losses,
+    }.to_json
+  )
   redis.quit
 end
 
